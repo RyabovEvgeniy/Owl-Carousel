614f9c4d0d93c7fdef01df43dc1e507aa20340c6
RyabovEvgeniy <likerus99@gmail.com>
2022-08-09 14:34:37 +0300

    Залил проект на github

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+		center: true,
+		loop: true,
+		items: 3,
+		margin: 30,
+		startPosition: 1,		
+	});
+});
+
+
+// $(document).ready(function () {
+// 	$('.slider').slick({
+// 		infinite: true,
+// 		slidesToShow: 3,
+// 		slidesToScroll: 1,
+// 		initialSlide: 1,
+// 	});
+// });
+	
